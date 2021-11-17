7d2c708be217ff0ebfb28a902a6a4fcb9d66fcf2
PawangRai <94522697+PawangRai@users.noreply.github.com>
2021-11-17 14:11:37 +0500

    These are the project files

---
+++ src/Breakout_Ball/Main.java
@@ -0,0 +1,17 @@
+package Breakout_Ball;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Gameplay gamePlay = new Gameplay();
+        JFrame obj = new JFrame();
+        obj.setBounds(10,10,700,600);
+        obj.setTitle("Breakout Ball");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gamePlay);
+    }
+}
